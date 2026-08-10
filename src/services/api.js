31436a8421c96d2cfdd2d165b0c5c89b9ec9d532
Pollyanna santos souza <141653239+pollyanna79@users.noreply.github.com>
2026-08-10 09:22:58 -0300

    Clean up api.js by removing placeholder comments
    Removed commented placeholders for Project URL and anon key.

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Cole sua Project URL aqui:
+
 const supabaseUrl = 'https://wdvtuvohucyndqjnfpyh.supabase.co';
 
-// Cole sua chave anon (a que você acabou de me enviar):
+
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndkdnR1dm9odWN5bmRxam5mcHloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNDk3ODgsImV4cCI6MjA4NjgyNTc4OH0.0Ep0zGoh_OJhPqVKZPhWjxL7CG26uuAJx7tGB5eeODM';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
